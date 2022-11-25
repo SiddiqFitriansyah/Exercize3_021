8b58aefb13e898328d20469c6a11b5de7084b706
SiddiqFitriansyah <siddiqfitriansyah14@gmail.com>
2022-11-25 16:15:23 +0700

    Create method Search

--- Exercize3_021/Program.cs
+++ Exercize3_021/Program.cs
@@ -9,5 +9,21 @@ namespace Exercize3_021
         public string name;
         public Node next;
     }
-
+    class CircularList
+    {
+        Node LAST;
+        public CircularList()
+        {
+            LAST = null;
+        }
+        public bool Search(int rollNo, ref Node previous, ref Node current)/*Search for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*Returns true if the node is found*/
+            }
+            
+        }
+    }
 }
