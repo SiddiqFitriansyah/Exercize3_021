dc8490c58bf7d6f91255f41e3f474ccdb74ca093
SiddiqFitriansyah <siddiqfitriansyah14@gmail.com>
2022-11-25 16:07:54 +0700

    Add project files.

---
+++ Exercize3_021/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercize3_021
+{
+    class Node
+    {
+        /*Create Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+
+}
